0faa16398a2c3e9ed50e23828f5a22673d3a632b
Lê Thiên <leeethien0612@gmail.com>
2025-12-02 02:29:24 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+﻿import os
+import json
+from flask import Flask, render_template, request
+from predictor import Predictor
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "static/uploads"
+
+# Load model và class names ngay trong thư mục webapp
+MODEL_PATH = "model_final.h5"
+predictor = Predictor(MODEL_PATH)
+
+with open("class_names.json", "r") as f:
+    class_names = json.load(f)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        img = request.files.get("image")
+        if img:
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], img.filename)
+            img.save(img_path)
+
+            result = predictor.predict(img_path, class_names)
+
+            return render_template(
+                "result.html",
+                img_path=img_path,
+                label=result["label"],
+                prob=round(result["prob"] * 100, 2)
+            )
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ predictor.py
@@ -0,0 +1,32 @@
+﻿# src/predictor.py
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from config import IMG_SIZE
+
+class Predictor:
+    def __init__(self, model_path):
+        print("🔹 Loading model:", model_path)
+        self.model = tf.keras.models.load_model(model_path)
+
+    def predict(self, img_path, class_names):
+        # Load image
+        img = image.load_img(img_path, target_size=IMG_SIZE)
+        img_arr = image.img_to_array(img)
+
+        # Preprocess theo EfficientNet
+        img_arr = preprocess_input(img_arr)
+
+        # Add batch dim
+        img_arr = np.expand_dims(img_arr, axis=0)
+
+        # Dự đoán
+        preds = self.model.predict(img_arr)
+        idx = np.argmax(preds)
+
+        return {
+            "label": class_names[idx],
+            "prob": float(preds[0][idx])
+        }
